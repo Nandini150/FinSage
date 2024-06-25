d042846b4c72c407560ffded6fae084a1720bffa
Nandini150 <80650101+Nandini150@users.noreply.github.com>
2024-06-25 21:14:43 +0530

    Your commit message here

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import Headerbox from '@/components/Headerbox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalancebox from '@/components/TotalBalancebox';
+// import React from 'react'
 
 const Home = () => {
+ const loggedIn={firstName:'Nandini',lastName:'Singh',email:'contact@nandinisingh.in'};  
   return (
-    <div>Home</div>
+   <section className="home">
+
+
+     <div className="home-conent">
+     <header className="home-header">
+      <Headerbox
+      type="greeting"
+      title="Welcome"
+      user={loggedIn?.firstName || 'Guest'}
+      subtext="Access and manage your account and transactions efficiently."
+  
+      
+      
+      />
+    
+
+<TotalBalancebox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.45}
+          />
+
+     </header>
+    RECENT TRANSACTIONS
+
+
+
+
+     </div>
+
+  <RightSidebar
+   user={loggedIn}
+   transactions={[]}
+   banks={[{currentBalance:123.50},{currentBalance:500.50}]}
+  />
+
+
+
+
+
+
+   </section>
+
+
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,20 @@
+'use client';
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}:{amount:number}) => {
+  return (
+    <div className="w-full">
+       <CountUp end={amount}
+       
+       duration={3}
+       decimals={2}
+        decimal=","
+        prefix="₹"
+       
+       
+       />
+    </div>
+  )
+}
+
+export default AnimatedCounter
